15e2584743adb7abf2586f57859d94043d1c293e
Angel ILIEV <a.v.iliev13@gmail.com>
2020-11-08 11:41:24 +0200

    Add singularity squares calculation & test.

--- src/server/game_engine/board.test.ts
+++ src/server/game_engine/board.test.ts
@@ -1,8 +1,9 @@
 import { Board } from "./board"
-import { Bishop } from "./pieces"
+import { Bishop, Knight } from "./pieces"
 import { Square } from "./square"
 
 test("Board isOccupied", () => {
+    // NOTE: this also checks Square.isContained functionality
     const board = new Board();
 
     const square = Square.fromNotation("a1");
@@ -18,5 +19,29 @@ test("Board isOccupied", () => {
         } else {
             expect(board.isOccupied(Square.fromIndex(i))).toBeTruthy();
         }
+    }
+});
+
+test("Board getSingularSquares", () => {
+    const board = new Board();
+
+    const squareBishop = Square.fromNotation("a1");
+    const bishop = new Bishop(squareBishop, board);
+
+    const squareKnight = Square.fromNotation("b1");
+    const knight = new Knight(squareKnight, board);
+
+    board.addPiece(bishop);
+    board.addPiece(knight);
+
+    const moves = board.getSingularSquares()
+    expect(moves.length).toBe(8);
+
+    // expect square which can be reached by both pieces to not be in the array of moves
+    expect(Square.fromNotation("c3").isContained(moves)).toBeFalsy();
+
+    const expectedMoves: string[] = ["b2", "d4", "e5", "f6", "g7", "h8", "a3", "d2"];
+    for (const expectedMove of expectedMoves) {
+        expect(Square.fromNotation(expectedMove).isContained(moves)).toBeTruthy();
     }
 });
--- src/server/game_engine/board.ts
+++ src/server/game_engine/board.ts
@@ -9,10 +9,12 @@ export class Board {
         this.pieces = [];
     }
 
+    /** Resets board (i.e. removes all pieces from the board). */
     reset(): void {
         this.pieces = [];
     }
 
+    /** Add a piece to the board */
     addPiece(piece: Piece): void {
         this.pieces.push(piece);
     }
@@ -22,4 +24,51 @@ export class Board {
         const occupiedSquares = this.pieces.map(piece => piece.square);
         return square.isContained(occupiedSquares);
     }
+
+    /** Return an array of squares to which only 1 piece can go. */
+    getSingularSquares(): Square[] {
+        // convert move arrays for each piece into a set
+        const allMoves = this.pieces.map(piece => new Set(piece.getMoves()));
+        
+        // Calculate the symmetric difference between all moves.
+        // This results in a set of squares which cannot be reached by more than one piece.
+        const singularSquaresSet: Set<Square> = allMoves.reduce(symmetricSquareDifference);
+
+        return Array.from(singularSquaresSet);
+    }
+}
+
+/** 
+ * Calculates symmetric difference between 2 sets of squares. 
+ * Returns the squares which are not overlapping between the two input sets. 
+ */
+const symmetricSquareDifference = (accumulator: Set<Square>, currentValue: Set<Square>, currentIndex, array) => {
+    // this implementation is taken directly from
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
+    let _difference = accumulator;
+
+    // conversion to array is needed in order to allow iteration over elements
+    for (let elem of Array.from(currentValue)) {
+        // convert to array for iteration and containment checks
+        const _differenceArray = Array.from(_difference);
+
+        // need to use specific check for presence because we are dealing with square objects
+        if (elem.isContained(_differenceArray)) {
+
+            // iterate over all objects in accumulator array and delete
+            // the one that is contained in the currentValue
+            // NOTE: need to do a custom comparison by index
+            //       because delete() will not find the matching
+            //       object to delete as we are dealing with non-basic types
+            for (const sq of _differenceArray) {
+                if (sq.index() === elem.index()) {
+                    _difference.delete(sq);
+                }
+            }
+
+        } else {
+            _difference.add(elem);
+        }
+    }
+    return _difference;
 }
