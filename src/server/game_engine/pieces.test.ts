7ac2489a77284e6026f2d04a84db93b64fe4c315
Angel ILIEV <a.v.iliev13@gmail.com>
2020-11-08 10:03:09 +0200

    Add queen moves test

--- src/server/game_engine/pieces.test.ts
+++ src/server/game_engine/pieces.test.ts
@@ -47,8 +47,8 @@ test("Knight moves (from d4) with blocker (on c2)", () => {
     const moves = knight.getMoves();
     expect(moves.length).toBe(7);
 
-    // expect c2 is not longer in move list
-    expect(Square.fromNotation("c2").isContained(moves)).toBeFalsy();
+    // expect blocking piece square is no longer in moves array
+    expect(bishop.square.isContained(moves)).toBeFalsy();
 
     const expectedMoves: string[] = ["b3", "b5", "c6", "e2", "e6", "f5", "f3"];
     for (const expectedMove of expectedMoves) {
@@ -104,9 +104,8 @@ test("King moves (from d4) with blocker (on c5)", () => {
     const moves = king.getMoves();
     expect(moves.length).toBe(7);
 
-
-    // expect c5 is not longer in move list
-    expect(Square.fromNotation("c5").isContained(moves)).toBeFalsy();
+    // expect blocking piece square is no longer in moves array
+    expect(bishop.square.isContained(moves)).toBeFalsy();
 
     const expectedMoves: string[] = ["c3", "c4", "d3", "d5", "e3", "e4", "e5"];
     for (const expectedMove of expectedMoves) {
@@ -168,14 +167,90 @@ test("Bishop moves (from d4) with blocker (on e5)", () => {
     const moves = bishop.getMoves();
     expect(moves.length).toBe(9);
 
-
-    // expect e5 is not longer in move list
-    expect(Square.fromNotation("e5").isContained(moves)).toBeFalsy();
+    // expect blocking piece square is no longer in moves array
+    expect(knight.square.isContained(moves)).toBeFalsy();
 
     const expectedMoves: string[] = [
         "c3", "b2", "a1", // bottom left ray
         "c5", "b6", "a7",  // top left ray
         "e3", "f2", "g1" // bottom right ray
+    ];
+    for (const expectedMove of expectedMoves) {
+        expect(Square.fromNotation(expectedMove).isContained(moves)).toBeTruthy();
+    }
+});
+
+// ----------------- Queen -----------------
+test("Queen moves (from a1)", () => {
+    const square = Square.fromNotation("a1");
+    const board = new Board();
+    const queen = new Queen(square, board);
+
+    expect(queen.square).toEqual(square);
+
+    const moves = queen.getMoves();
+
+    expect(moves.length).toBe(21);
+    const expectedMoves: string[] = [
+        "a2", "a3", "a4", "a5", "a6", "a7", "a8", // north vector 
+        "b2", "c3", "d4", "e5", "f6", "g7", "h8", // north-east diagonal
+        "b1", "c1", "d1", "e1", "f1", "g1", "h1", // east vector
+    ];
+    for (const expectedMove of expectedMoves) {
+        expect(Square.fromNotation(expectedMove).isContained(moves)).toBeTruthy();
+    }
+});
+
+test("Queen moves (from d4)", () => {
+    const square = Square.fromNotation("d4");
+    const board = new Board();
+    const queen = new Queen(square, board);
+
+    expect(queen.square).toEqual(square);
+
+    const moves = queen.getMoves();
+    expect(moves.length).toBe(27);
+
+    const expectedMoves: string[] = [
+        "a1", "b2", "c3", "e5", "f6", "g7", "h8", // north-east diagonal
+        "a7", "b6", "c5", "e3", "f2", "g1", // north-west diagonal
+        "d8", "d7", "d6", "d5", "d3", "d2", "d1", // north-south vector
+        "a4", "b4", "c4", "e4", "f4", "g4", "h4", // north-south vector
+    ];
+
+    for (const expectedMove of expectedMoves) {
+        expect(Square.fromNotation(expectedMove).isContained(moves)).toBeTruthy();
+    }
+});
+
+test("Queen moves (from d4) with blockers (on c4 & e3)", () => {
+    const board = new Board();
+
+    const queenSquare = Square.fromNotation("d4");
+    const queen = new Queen(queenSquare, board);
+
+    const knightSquare1 = Square.fromNotation("c4");
+    const knight1 = new Knight(knightSquare1, board);
+
+    const knightSquare2 = Square.fromNotation("e3");
+    const knight2 = new Knight(knightSquare2, board);
+
+    // add blocking pieces to the board
+    board.addPiece(knight1);
+    board.addPiece(knight2);
+
+    const moves = queen.getMoves();
+    expect(moves.length).toBe(21);
+
+    // expect blocking piece squares are no longer in move list
+    expect(knight1.square.isContained(moves)).toBeFalsy();
+    expect(knight2.square.isContained(moves)).toBeFalsy();
+
+    const expectedMoves: string[] = [
+        "a1", "b2", "c3", "e5", "f6", "g7", "h8", // north-east diagonal
+        "a7", "b6", "c5",  // north-west diagonal
+        "d8", "d7", "d6", "d5", "d3", "d2", "d1", // north-south vector
+        "e4", "f4", "g4", "h4", // north-south vector
     ];
     for (const expectedMove of expectedMoves) {
         expect(Square.fromNotation(expectedMove).isContained(moves)).toBeTruthy();
