0c97c2b16068f31fa526a0580ae3910a67422510
Angel ILIEV <a.v.iliev13@gmail.com>
2020-11-15 12:33:57 +0200

    Replaced engine parts with chessjs.

--- src/client/src/game_engine/board.ts
+++ src/client/src/game_engine/board.ts
@@ -1,50 +1,63 @@
-import { Piece } from "./pieces"
-import { Square } from "./square"
+import { Chess, ChessInstance, Piece, PieceType, Square } from "chess.js"
+import { SquareFunctions } from "./square"
 
 
 export class Board {
-    pieces: Piece[];
+    chess: ChessInstance;
 
     constructor() {
-        this.pieces = [];
+        this.chess = new Chess();
+        this.chess.clear(); // remove all pieces from the board
     }
 
     /** Resets board (i.e. removes all pieces from the board). */
     reset(): void {
-        this.pieces = [];
+        this.chess.clear();
     }
 
     /** Add a piece to the board */
-    addPiece(piece: Piece): void {
-        this.pieces.push(piece);
+    addPiece(piece: PieceType, square: Square): void {
+        // always add the same pice color, otherwise move generation
+        // might generate some capture moves
+        const pieceObj: Piece = {type: piece, color: this.chess.WHITE}
+        this.chess.put(pieceObj, square);
     }
 
     /** Checks if a given square is already occupied by a piece or not. */
     isOccupied(square: Square): boolean {
-        const occupiedSquares = this.pieces.map(piece => piece.square);
-        return square.isContained(occupiedSquares);
+        const pieceAtSquare = this.chess.get(square);
+        return pieceAtSquare !== null;
     }
 
     /** Return an array of squares to which only 1 piece can go. */
     getSingularSquares(): Square[] {
-        // convert move arrays for each piece into a set
-        const allMoves = this.pieces.map(piece => new Set(piece.getMoves()));
-        
-        // Calculate the symmetric difference between all moves.
+        let allMoves: Array<Set<Square>> = [];
+
+        // Iterate over all piece on the board
+        // For every piece add the set of squares it can go to to an array
+        for (let i = 0; i < 64; i++) {
+            const square = SquareFunctions.fromIndex(i);
+
+            if (this.isOccupied(square)) {
+                const moves = this.chess.moves({ verbose: true, square: square });
+                allMoves.push(new Set(moves.map(move => move.to)));
+            }
+        }
+
+        // Calculate the symmetric difference between all square.
         // This results in a set of squares which cannot be reached by more than one piece.
         const singularSquaresSet: Set<Square> = allMoves.reduce(symmetricSquareDifference);
-
         return Array.from(singularSquaresSet);
     }
 
     /** Get piece object that can reach the given square. */
-    getPieceThatReachesSquare(square: Square): Piece {
-        for (const piece of this.pieces) {
-            if (square.isContained(piece.getMoves())) {
-                return piece;
+    getPieceThatReachesSquare(square: Square): PieceType | null {
+        const allMoves = this.chess.moves({ verbose: true });
+        for (const move of allMoves) {
+            if (move.to === square) {
+                return move.piece;
             }
         }
-        // TODO: should this throw error or return null ?
         return null;
     }
 }
@@ -53,30 +66,14 @@ export class Board {
  * Calculates symmetric difference between 2 sets of squares. 
  * Returns the squares which are not overlapping between the two input sets. 
  */
-const symmetricSquareDifference = (accumulator: Set<Square>, currentValue: Set<Square>, currentIndex, array) => {
+const symmetricSquareDifference = (accumulator: Set<Square>, currentValue: Set<Square>, currentIndex: any, array: any) => {
     // this implementation is taken directly from
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
-    let _difference = accumulator;
+    let _difference = new Set(accumulator);
 
-    // conversion to array is needed in order to allow iteration over elements
-    for (let elem of Array.from(currentValue)) {
-        // convert to array for iteration and containment checks
-        const _differenceArray = Array.from(_difference);
-
-        // need to use specific check for presence because we are dealing with square objects
-        if (elem.isContained(_differenceArray)) {
-
-            // iterate over all objects in accumulator array and delete
-            // the one that is contained in the currentValue
-            // NOTE: need to do a custom comparison by index
-            //       because delete() will not find the matching
-            //       object to delete as we are dealing with non-basic types
-            for (const sq of _differenceArray) {
-                if (sq.index() === elem.index()) {
-                    _difference.delete(sq);
-                }
-            }
-
+    for (let elem of currentValue) {
+        if (_difference.has(elem)) {
+            _difference.delete(elem);
         } else {
             _difference.add(elem);
         }
--- src/client/src/game_engine/square.ts
+++ src/client/src/game_engine/square.ts
@@ -1,93 +1,30 @@
-/** Square color enum */
-export enum Color {
-    White,
-    Black
-}
+import { Square } from "chess.js"
 
+export class SquareFunctions {
+    static FILES: string[] = ["a", "b", "c", "d", "e", "f", "g", "h"];
+    // create an array of numbers from 1-8 (inclusive)
+    static RANKS: number[] = Array.from(new Array(8), (x, i) => i + 1);
 
-const FILES: string[] = ["a", "b", "c", "d", "e", "f", "g", "h"];
-// create an array of numbers from 1-8 (inclusive)
-const RANKS: number[] = Array.from(new Array(8), (x, i) => i + 1);
-// 32-bit bitmask to determine the color of squares (all 1's represent black)
-const COLOR_BITBOARD: number = 0xAA55AA55;
+    static toIndex(square: Square): number {
+        let squareStr = square as string;
+        squareStr = squareStr.toLowerCase();
 
+        let fileStr = squareStr[0];
+        let rankStr = squareStr[1];
 
-export class Square {
-    file: number;
-    rank: number;
+        let file = this.FILES.indexOf(fileStr);
+        let rank = this.RANKS.indexOf(parseInt(rankStr));
 
-    constructor(file: number, rank: number) {
-        if (!(0 <= file && file < 8) || !(0 <= rank && rank < 8)) {
-            throw new Error(`Ivalid rank/file: f-${file} r-${rank}`);
-        }
-
-        this.file = file;
-        this.rank = rank;
+        return (rank * 8) + file;
     }
 
-    /** Alternative constructor for creating Square objects from 64-based index*/
     static fromIndex(index: number): Square {
         if (!(0 <= index && index < 64)) {
             throw new Error(`Wrong index: ${index}. Index should be in range [0, 63]`);
         }
         let rank = Math.floor(index / 8);
         let file = index % 8;
-        return new this(file, rank);
-    }
-
-    /** Alternative constructor for creating Square objects from square notation (i.e. "a1", "d3"*/
-    static fromNotation(notation: string): Square {
-        if (notation.length !== 2) {
-            throw new Error(`Wrong square format ${notation}`);
-        }
-
-        notation = notation.toLowerCase();
-
-        let fileStr = notation[0];
-        let rankStr = notation[1];
-
-        if (!(FILES.includes(fileStr)) || !(RANKS.includes(parseInt(rankStr)))) {
-            throw new Error(`Wrong square format ${notation}`);
-        }
-
-        let file = FILES.indexOf(fileStr);
-        let rank = RANKS.indexOf(parseInt(rankStr));
-
-        return new this(file, rank);
-    }
-
-    /** Return a 64-based index of the square */
-    index(): number {
-        return (this.rank * 8) + this.file;
-    }
-
-    /** Return a notation string of the square position (ex. a1, d3 etc.) */
-    notation(): string {
-        return `${FILES[this.file]}${RANKS[this.rank]}`;
-    }
-
-    /** Return an enum value of the color of the square: Black or White */
-    color(): Color {
-        // Convert index to 32-bit representation since the board pattern is the same
-        let index = this.index();
-        if (index >= 32) {
-            index = index - 32;
-        }
-
-        // Compute if square is black or white based on index 
-        // and its intersection to the color bitmask
-        const isBlack: number = (COLOR_BITBOARD >> index) & 1;
-
-        return (isBlack ? Color.Black : Color.White);
-    }
-
-    /** Check if this square is contained in an array of squares. */
-    isContained(squares: this[]): boolean {
-        for (const square of squares) {
-            if (square.index() === this.index()) {
-                return true;
-            }
-        }
-        return false;
+        let square = `${this.FILES[file]}${this.RANKS[rank]}`;
+        return square as Square;
     }
 }
