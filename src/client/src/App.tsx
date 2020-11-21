589401183f55488271b9fd8002c929f9d1398a66
BairnOwl <linda.chang.ace@gmail.com>
2020-11-21 08:42:04 -0500

    added chessboard.js library

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -2,25 +2,30 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Chessboard from 'chessboardjsx';
+import { Chess } from 'chess.js';
+
+class App extends React.Component {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      boardPosition: 'start'
+    }
+  }
+
+  componentDidMount() {
+    // this.setState({
+    //   board: Chessboard('chess-board', 'start')
+    // });
+  }
+
+  render() {
+    return (
+      <Chessboard position="{this.state.boardPosition}" />
+    );
+  }
+
 }
 
 export default App;
